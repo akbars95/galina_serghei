3d091d8206266c76d251714c55eade38e34d1570
mtsmda <mynzat.dmitrii@gmail.com>
2023-09-21 12:06:13 +0300

    changed project name in angular.json changed base href

--- galina_serghei/shell/build.sh
+++ galina_serghei/shell/build.sh
@@ -3,5 +3,5 @@
 cd ..
 rm -rfv ../docs
 #ng build --output-path for_publish
-ng build --output-path ../docs --base-href ./galina_serghei/
+ng build --output-path ../docs --base-href ./galina_serghei
 cd shell
